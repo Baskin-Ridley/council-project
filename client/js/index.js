75278224219d987bb1a91a503d1bc807028c90ed
Sadik Islam <sadikislam46@gmail.com>
2023-02-14 15:19:37 +0000

    frontend of login/signup page

--- client/js/index.js
+++ client/js/index.js
@@ -1,1 +1,12 @@
-console.log("JavaScript Linked!")
+let clickSignup = () => {
+    document.querySelector(".login").style = "transform:translateX(-90%);";
+    document.querySelector(".signup").style = "transform:translateX(0);";
+    document.querySelector("#undertab").style = "left:null;right:0;";
+  };
+  
+  let clickLogin = () => {
+    document.querySelector(".login").style = "transform:translateX(0)";
+    document.querySelector(".signup").style = "transform:translateX(90%);";
+    document.querySelector("#undertab").style = "right:null; left:0;";
+  };
+  
