7f428daa597387fb3aa7feaa2a88762d36914aa7
shojib deb <shodeb123@gmail.com>
2023-02-14 17:14:31 +0000

    fix jwt tokenization

--- server/db/users.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-    id SERIAL NOT NULL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    profile_pic VARCHAR(255),
-    job_id VARCHAR(255) []
-);
