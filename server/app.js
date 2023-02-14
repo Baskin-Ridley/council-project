7671594f61af5bee148953814f9815b784bc7894
Baskin-Ridley <gbaskinridley@gmail.com>
2023-02-14 10:34:46 +0000

    update app

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const dotenv = require('dotenv').config();
 
 // Serve static files from the 'client' directory
 app.use(express.static(path.join(__dirname, '../client'), {
@@ -18,6 +19,8 @@ app.use(express.static(path.join(__dirname, '../client'), {
 }));
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
