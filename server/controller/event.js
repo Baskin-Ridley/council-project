5a302335eaba2a3c6baef69b4ab3c3630f7cc5ce
Sadik Islam <sadikislam46@gmail.com>
2023-02-15 20:55:02 +0000

    working on events

--- server/controller/event.js
+++ server/controller/event.js
@@ -1,15 +1,36 @@
+const { restart } = require("nodemon");
 const {Event} = require("../db/event");
 
 // get all events
-async function getAllEvents() {
-    const events = await Event.findAll();
-    return events;
+async function getAllEvents(req, res) {
+    try {
+        const result = await Event.getAllEvents();
+        res.status(200).json(result);
+    } catch(err) {
+        res.status(404).json({
+            error: true,
+            message: err.message
+        });
+    }
 }
 
 // create a new event
-async function createEvent(eventData) {
-    const event = await Event.create(eventData);
-    return event;
+async function createEvent(req, res) {
+    const data = req.body;
+    
+    try {
+        if (["e_id", "e_name", "e_date", "e_time", "e_location"].every(key => Object.hasOwn(data, key))) {
+            const post = await Event.create(data);
+            res.status(201).json({message: "post created successfully"})
+        } else {
+            throw new Error("Invalid properties")
+        }
+    } catch(err) {
+        res.status(404).json({
+            error: true,
+            message: err.message
+        });
+    }
 }
 
 module.exports = {
