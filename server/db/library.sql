1f890ba7fc2814f3292b26a692e6327c5f690556
shojib deb <shodeb123@gmail.com>
2023-02-15 10:25:23 +0000

    marketplace model

--- server/db/library.sql
+++ server/db/library.sql
@@ -1,7 +1,6 @@
 CREATE TABLE library (
     library_id SERIAL NOT NULL,
     posting_date DATE DEFAULT (current_timestamp AT TIME ZONE 'GMT'),
-    VARCHAR(500) NOT NULL,
     title VARCHAR(500) NOT NULL,
     img_url VARCHAR(500),
     activity_date DATE,
