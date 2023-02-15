e4011d1169425dbd52184875447b67adcc032d5a
Baskin-Ridley <gbaskinridley@gmail.com>
2023-02-15 12:17:59 +0000

    add books to seed

--- server/db/library.sql
+++ server/db/library.sql
@@ -9,3 +9,9 @@ CREATE TABLE library (
     PRIMARY KEY(library_id),
     CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
+-- Insert sample data into the library table
+INSERT INTO library (title, img_url, user_id, content) VALUES
+  ('The Great Gatsby', 'https://placebear.com/200/300', 1, 'The Great Gatsby is a 1925 novel by American writer F. Scott Fitzgerald.'),
+  ('To Kill a Mockingbird', 'https://placebear.com/200/200', 2, 'To Kill a Mockingbird is a novel by Harper Lee published in 1960.'),
+  ('1984', 'https://placebear.com/200/300', 3, '1984 is a dystopian social science fiction novel by English novelist George Orwell.');
