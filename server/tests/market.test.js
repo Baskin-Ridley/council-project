2817486904fbc5a9ccd2a0d968acf17f6d5413b8
Baskin-Ridley <gbaskinridley@gmail.com>
2023-02-16 15:34:15 +0000

    add tests

--- server/tests/market.test.js
+++ server/tests/market.test.js
@@ -18,3 +18,47 @@ describe("newItemListing", () => {
     expect(newItemListing).toBeDefined();
   });
 });
+
+describe('deletePost', () => {
+    beforeEach(() => {
+      fetchMock.resetMocks();
+    });
+  
+    it("exists", () => {
+        expect(deletePost).toBeDefined();
+    });
+
+    it('sends the correct data to the server', async () => {
+      fetchMock.mockResponseOnce(JSON.stringify({}));
+  
+      const postId = 123;
+      await deletePost(postId);
+  
+      expect(fetchMock.mock.calls.length).toBe(1);
+      expect(fetchMock.mock.calls[0][0]).toBe('http://localhost:3000/market/delete');
+      expect(fetchMock.mock.calls[0][1].method).toBe('POST');
+      expect(fetchMock.mock.calls[0][1].headers['Content-Type']).toBe('application/json');
+      expect(JSON.parse(fetchMock.mock.calls[0][1].body)).toEqual({ marketplace_id: postId });
+    });
+  /* currently does not work
+    it('reloads the page after deleting the post', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({}));
+      
+        const reloadMock = jest.fn();
+        Object.defineProperty(window, 'location', {
+          value: { reload: reloadMock }
+        });
+      
+        await deletePost(456);
+      
+        expect(reloadMock).toHaveBeenCalled();
+      });
+      */
+  });
+
+describe("newPostPopup", () => {
+    it("exists", () => {
+        expect(newPostPopup).toBeDefined();
+    });
+});
+
