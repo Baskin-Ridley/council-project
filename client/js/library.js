f88e71096f1184632bbbaa9baee05e917718d081
Baskin-Ridley <gbaskinridley@gmail.com>
2023-02-16 09:34:14 +0000

    add userId to each page

--- client/js/library.js
+++ client/js/library.js
@@ -1,3 +1,12 @@
+const getPayload = () => {
+    const token = window.localStorage.getItem("token")
+    if(!token) return false
+    const parts = token.split(".")
+    if (parts.length < 3) return false
+    return JSON.parse(atob(parts[1]))
+  }
+  let userId = getPayload().sub
+  console.log(userId)
 
 const calendar = document.getElementById('calendar');
 const newEventPopup = document.getElementById('newEventPopup');
